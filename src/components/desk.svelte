f9506b804394b4219af5db688fd8c3e36e175d38
kyle-barth <kylebarth@me.com>
2021-06-23 12:18:47 +0100

    refactor / code cleanup for 3D desk

--- src/components/desk.svelte
+++ src/components/desk.svelte
@@ -9,6 +9,8 @@
 	import { slideL } from '../animations/slide';
 	import Divider from './divider.svelte';
 
+	import Lightbulb from '../icons/lightbuld.svg.svelte';
+
 	let el: HTMLCanvasElement;
 	export let loaded: boolean;
 
@@ -16,6 +18,25 @@
 	let resetControls = () => {};
 
 	onMount(() => {
+		const loader = new GLTFLoader();
+		const scene = new THREE.Scene();
+
+		let renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
+
+		const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+		camera.position.set(-55, 55, 55);
+		const OrbitControls = oc(THREE);
+		const controls = new OrbitControls(camera, renderer.domElement);
+		controls.maxDistance = 125;
+		controls.autoRotate = true;
+		controls.enableRotate = true;
+		controls.enablePan = false;
+		controls.enableZoom = true;
+
+		resetControls = () => {
+			controls.reset();
+		};
+
 		const ambientlight = new THREE.AmbientLight(0x404040, 5);
 
 		const clearLights = () => {
@@ -36,23 +57,10 @@
 			}
 
 			const randomColor: string = Math.floor(Math.random() * 16777215).toString(16);
-			const randomIntensity: number = Math.ceil(Math.random() * 5);
+			const randomIntensity: number = Math.ceil(Math.random() * 10);
 			const pointlight = new THREE.PointLight(`#${randomColor}`, randomIntensity, 100);
 			pointlight.position.set(0, 50, 0);
 			scene.add(pointlight);
-		};
-
-		const loader = new GLTFLoader();
-		const scene = new THREE.Scene();
-
-		let renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
-
-		const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-		camera.position.set(-55, 55, 55);
-		const OrbitControls = oc(THREE);
-		const controls = new OrbitControls(camera, renderer.domElement);
-		resetControls = () => {
-			controls.reset();
 		};
 
 		loader.load(
@@ -81,11 +89,6 @@
 				resize();
 				animate();
 
-				controls.autoRotate = true;
-				// controls.enableRotate = false;
-				// controls.enablePan = false;
-				// controls.enableZoom = false;
-
 				window.addEventListener('resize', resize);
 
 				el.classList.add('opacity-100');
@@ -101,7 +104,7 @@
 
 <div class="absolute overflow-hidden">
 	<canvas
-		class="transform translate-x-1/5 pt-10 transition-all duration-700 scale-0 opacity-0"
+		class="transform translate-x-1/5 transition-all duration-700 scale-0 opacity-0"
 		bind:this={el}
 	/>
 </div>
@@ -113,20 +116,7 @@
 			class="text-gray-400 font-mono cursor-pointer focus:outline-none flex items-center hover:underline"
 		>
 			randomise lighting
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-8 w-8 pl-2"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-				/>
-			</svg>
+			<Lightbulb />
 		</button>
 
 		<Divider />
