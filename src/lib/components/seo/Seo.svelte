8c31270d52a58dc4bf1ce14db2679f70870f3bdf
Kyle Barth <kylebarth@me.com>
2022-02-20 22:15:22 +0000

    remove SEO dynamic behaviour from component

--- src/lib/components/seo/Seo.svelte
+++ src/lib/components/seo/Seo.svelte
@@ -1,17 +1,4 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
-
-	let path;
-
-	onMount(() => {
-		page.subscribe((page) => {
-			path = page.url.pathname.replace('/', '');
-		});
-	});
-</script>
-
 <svelte:head>
 	<meta name="description" content="Kyle Barth - Web Developer - Portfoilio website." />
-	<title>kyle-barth {path && `| ${path}`}</title>
+	<title>kyle-barth.dev</title>
 </svelte:head>
